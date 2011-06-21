c7a7f0a1a7c1fb6cf708856689b605f530e65bc2
Uberto Barbini <uberto.gama@gmail.com>
2011-06-21 11:03:03 +0200

    finished LongMethod problem

--- longMethod/src/main/com/tsumego/longMethod/Client.java
+++ longMethod/src/main/com/tsumego/longMethod/Client.java
@@ -1,7 +1,5 @@
 package com.tsumego.longMethod;
 
-import java.lang.String;
-
 public class Client {
 
     private String firstName;
@@ -12,16 +10,29 @@ public class Client {
 
 
     public Client(String fullName) {
+        int firstSep = -1;
+        int secondSep = -1;
 
-        for(int i = 0; i < fullName.length(); i++){
+        for (int i = 0; i < fullName.length(); i++) {
 
-            if (fullName.charAt(i) == ' '){
+            if (fullName.charAt(i) == ' ') {
+                if (firstSep == -1) {
+                    firstSep = i;
+                } else {
+                    secondSep = i;
+                }
 
             }
         }
 
-        this.firstName = firstName;
-        this.lastName = lastName;
+        if (secondSep == -1) {
+            this.firstName = fullName.substring(0, firstSep);
+            this.lastName = fullName.substring(firstSep + 1);
+        } else {
+            this.firstName = fullName.substring(0, firstSep);
+            this.middleName = fullName.substring(firstSep + 1, secondSep);
+            this.lastName = fullName.substring(secondSep + 1);
+        }
     }
 
     public String getFirstName() {
--- removeIfAndNull/src/test/com/tsumego/removeIfAndNull/ClientTest.java
+++ removeIfAndNull/src/test/com/tsumego/removeIfAndNull/ClientTest.java
@@ -11,11 +11,15 @@ public class ClientTest {
     @Test
     public void shouldComposeLongName() throws Exception {
         Client client = new Client("John", "Long", "Silver");
+        assertThat(client.getFirstName(), is("John"));
+        assertThat(client.getMiddleName(), is("Long"));
+        assertThat(client.getLastName(), is("Silver"));
+
         assertThat(client.getFullName(), is("John Long Silver"));
 
     }
 
-        @Test
+    @Test
     public void shouldComposeShortName() throws Exception {
         Client client = new Client("Homer", null, "Simpson");
         assertThat(client.getFullName(), is("Homer Simpson"));
